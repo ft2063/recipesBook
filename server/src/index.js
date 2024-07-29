f3c282ecc91371d8bf424523f301189a71eebffa
Flavia Trotolo <72688128+ft2063@users.noreply.github.com>
2024-07-29 13:19:21 +0400

    add recipes to models

--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,14 @@ import express from "express";
 import cors from 'cors';
 import mongoose from 'mongoose';
 import {userRouter} from './routes/users.js';
+import {recipeRouter} from './routes/recipes.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use("/auth", userRouter);
-
+app.use("/recipes", recipeRouter);
 mongoose.connect(
     "mongodb+srv://flaviatrotolo:ABy8mzRtPAP9pzO3@recipies.ckaghac.mongodb.net/recipies?retryWrites=true&w=majority&appName=recipies"
 );
